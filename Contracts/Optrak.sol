4804cc17f0c88ad4dc5f165eabe9c55db1941afa
Guillermo Leon <leon.guillermo1499@gmail.com>
2018-05-24 12:27:26 -0700

    Optrak Skeleton imported into newest version

--- Contracts/Optrak.sol
+++ Contracts/Optrak.sol
@@ -2,6 +2,80 @@ pragma solidity ^0.4.23;
 
 import "./Ownable.sol";
 
-contract Optrak is Ownable{
+contract Optrak is Ownable {
+    // -------------------- Provider Registry ----------------------------------
+    mapping(string=>string) provider2pubkey;
+    // uint public totalProviderCount;
+    // todo: save provider index and create an actual provider-pubkey object in server code
+    // mapping(uint=>string) index2provider; // hide this mapping so it's difficult to retrieve providers without index
+    // -------------------- End of Provider Registry ---------------------------
+
+    // -------------------- Shared Meta Data -----------------------------------
+    mapping(string=>mapping(string=>string)) provider2meta; // {provider: {type/name: api/pointer}}
+    mapping(string=>uint) provider2metaCount;               // {provider: #}
+    mapping(string=>mapping(uint=>string)) index2metaName;  // {provider: {#: type/name}}
+    // note: could create the following mapping (existMetaType) in app code via traverse of previous data to save contract space
+    // but each time a new meta data is added, traverse function has to be called
+    // mapping(string=>mapping(string=>bool)) existMetaType;// {provider: {type/name: bool}} - use string comparison instead
+    // -------------------- End of Shared Meta Data ----------------------------
+
+    // -------------------- Meta Data Access------- ----------------------------
+    mapping(string=>mapping(string=>mapping(string=>bool))) metadata2access; // {provider1:{metaname: {provider2: bool}}}
+    // -------------------- End of Meta Data Access ----------------------------
+
+    function getProviderPubkey(string provider) public view returns(string) {
+        // string storage provider = index2provider[index];
+        // todo: in server code check returned value against null
+        return provider2pubkey[provider];
+    }
+
+    // Current function assumes one provider corresponds to one pubkey and overwrites pubkey if provider exists
+    function addProvider(string provider, string pubkey) public onlyOwner() {
+        // bytes memory tempExistingProvider = bytes(provider2pubkey[provider]);
+        // if (tempExistingProvider.length == 0) {
+            // index2provider[totalProviderCount] = provider;
+            // ++totalProviderCount;
+        // }
+        provider2pubkey[provider] = pubkey;
+    }
+
+    function getProviderMetaCount(string provider) public view returns(uint) {
+        return provider2metaCount[provider];
+    }
+
+    function getMetaName(string provider, uint index) public view returns(string) {
+        return index2metaName[provider][index];
+    }
+    
+    function getMetaData(string provider, string metaName) public view returns(string) {
+        return provider2meta[provider][metaName];
+    }
+
+    function addMetaData(string provider, string metaName, string content, bool overwrite) public onlyOwner() returns(bool){
+        
+        bytes memory tempExistingMeta = bytes(provider2meta[provider][metaName]);
+        if (tempExistingMeta.length == 0) {
+            index2metaName[provider][provider2metaCount[provider]] = metaName;
+            provider2meta[provider][metaName] = content;
+            ++provider2metaCount[provider];
+        } else if (overwrite) {
+            provider2meta[provider][metaName] = content;
+        } else {
+            //Prompt user on frontend for permission to overwrite current metadata
+            return false;
+        }
+        return true;
+    }
+
+    function getMetaDataAccess(string sharer, string metaName, string sharee) public view returns(bool) {
+        return metadata2access[sharer][metaName][sharee];
+    }
+
+    function updateMetaDataAccess(string sharer, string metaName, string sharee, bool access) public onlyOwner() returns(bool) {
+        bytes memory tempExistingMeta = bytes(getMetaData(sharer, metaName));
+        if (tempExistingMeta.length == 0) return false;
+        metadata2access[sharer][metaName][sharee] = access;
+        return true;
+    }
 
 }
