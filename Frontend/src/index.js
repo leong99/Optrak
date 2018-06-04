851ec427d94c54458882cf5537991c09ba5e9a02
Guillermo Leon <leon.guillermo1499@gmail.com>
2018-06-04 14:55:13 -0500

    Handled redirects in index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './Components/App';
 import SignIn from './Components/SignIn';
 import SignUp from './Components/SignUp';
-import {BrowserRouter, Route, browserHistory} from 'react-router-dom';
+import {BrowserRouter, Route, browserHistory, withRouter} from 'react-router-dom';
 import {firebaseApp} from './firebase';
+import history from './history';
 
 firebaseApp.auth().onAuthStateChanged(user => {
+    
     if(user) {
         console.log('user signed in/up', user);
+        history.push('/app');
     }
     else {
         console.log('user has signed out, or still needs to sign in');
+        history.push('/signin');
     }
 })
 
@@ -24,3 +28,4 @@ ReactDOM.render(
         </div>
     </BrowserRouter>, document.getElementById('root')
 );
+
