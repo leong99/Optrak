ff9028cb64d1eacbdc6e630b614893c5064463b0
timothycobrien <timothy@obrienlighting.com>
2018-06-04 12:23:12 -0500

    Trying to implement some sort of sign in process

--- Frontend/src/Components/SignIn.jsx
+++ Frontend/src/Components/SignIn.jsx
@@ -2,9 +2,25 @@ import React, {Component} from 'react';
 
 class SignIn extends Component {
     render() {
-        return (
-        <div> SignIn </div>    
-        );
+        {/* <div className="form-inline">
+            <h2>Sign-in to OpTrak</h2>
+            <div className="form-group">
+                
+                <input className="form-control" type="text" placeholder="Name" onChange ={event => this.setState({name: event.target.value})}/>
+                <input
+                 className="form-control" 
+                 type="password" 
+                 placeholder="Public Key"
+                 onChange={event =>
+                this.setState({pubkey: event.target.value})}/>
+                <button className="btn btn-primary" 
+                type="button"
+                onClick={() => this.signUp()}
+                > 
+                Complete Registration
+                </button>
+            </div>
+        </div> */}
     }
 }
 
