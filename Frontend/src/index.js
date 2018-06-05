fbbb6fc776ee5e73913d1e10b5d900da62a5a3f7
Guillermo Leon <leon.guillermo1499@gmail.com>
2018-06-05 11:27:37 -0500

    Added reducers and actions

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,19 +3,23 @@ import ReactDOM from 'react-dom';
 import App from './Components/App';
 import SignIn from './Components/SignIn';
 import SignUp from './Components/SignUp';
-<<<<<<< HEAD
 import {BrowserRouter, Route, browserHistory, withRouter} from 'react-router-dom';
-=======
-import Temp from './Components/Temp';
-import {BrowserRouter, Route, browserHistory} from 'react-router-dom';
->>>>>>> 258e27254dcea645f3db697585482c621768c939
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers'
+import {logUser} from './actions';
+
 import {firebaseApp} from './firebase';
 import history from './history';
+
+const store = createStore(reducer);
 
 firebaseApp.auth().onAuthStateChanged(user => {
     
     if(user) {
         console.log('user signed in/up', user);
+        const email = user;
+        store.dispatch(logUser(email));
         history.push('/app');
     }
     else {
@@ -25,13 +29,14 @@ firebaseApp.auth().onAuthStateChanged(user => {
 })
 
 ReactDOM.render(
-    <BrowserRouter path="/">
-        <div>
-            <Route path="/app" component={App} />
-            <Route path="/signin" component={SignIn}/>
-            <Route path="/signup" component={SignUp}/>
-            <Route path="/temp" component={Temp}/>
-        </div>
-    </BrowserRouter>, document.getElementById('root')
+    <Provider store={store}>
+        <BrowserRouter path="/">
+            <div>
+                <Route path="/app" component={App} />
+                <Route path="/signin" component={SignIn}/>
+                <Route path="/signup" component={SignUp}/>
+            </div>
+        </BrowserRouter>
+    </Provider>, document.getElementById('root')
 );
 
