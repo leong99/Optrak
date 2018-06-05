30210849b5ebdf6c759ab57e5ebe3dcb78e32519
timothycobrien <timothy@obrienlighting.com>
2018-06-05 18:13:49 -0500

    Added sign in functionality using firebase for email verification

--- Frontend/src/Components/SignIn.jsx
+++ Frontend/src/Components/SignIn.jsx
@@ -1,8 +1,52 @@
 import React, {Component} from 'react';
+import {firebaseApp} from '../firebase';
+import { Link, BrowserRouter, Route } from 'react-router-dom';
 
 class SignIn extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            email: '',
+            password: '',
+            error:{
+                message:''
+            }
+        }
+    }
+    signIn(){
+        console.log('this.state', this.state);
+        const {email, password}=this.state;
+        firebaseApp.auth().signInWithEmailAndPassword(email, password).catch(error =>{
+                this.setState({error})
+            })
+    }
+
     render() {
-        return <div> SignIN </div>;
+        return (
+            <div className="form-inline">
+                <h2>Sign in to Optrak</h2>
+                <div className="form-group">
+                    <input
+                     className="form-control" 
+                     type="text" 
+                     placeholder="Email Address"
+                     onChange={event => this.setState({email: event.target.value})}/>
+                     <input
+                     className="form-control" 
+                     type="password" 
+                     placeholder="Password"
+                     onChange={event => this.setState({password: event.target.value})}/>
+                    <button className="btn btn-primary" 
+                    type="button"
+                    onClick={() => this.signIn()}
+                    >
+                    Complete Registration
+                    </button>
+                </div>
+                <div>{this.state.error.message}</div>
+                <div><Link to={'/signup'}> Sign up instead </Link> </div>
+            </div>
+            );
     }
 }
 
