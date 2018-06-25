9b112a64b36ee973cc735665f1d82e235a13f641
timothycobrien <timothy@obrienlighting.com>
2018-06-25 13:16:58 -0500

    Rendered list of patients

--- Frontend/src/Components/AccessData.jsx
+++ Frontend/src/Components/AccessData.jsx
@@ -11,36 +11,61 @@ class AccessData extends Component {
         this.state = {
             userName: '',
             patientName: '',
-            error:{
-                message:'Enter a registered patient'
+            patients: '',
+            clicked: false,
+            error: {
+                message: 'Enter a registered patient'
             }
         };
     }
 
-    async queryData(userName, patientName){
-        var query=firebaseApp.database().ref(`Users/${this.state.userName}/Patients/${this.state.patientName}`);
-        var dataArr=new Array();
+    async queryData(userName, patientName) {
+        var query = firebaseApp.database().ref(`Users/${this.state.userName}/Patients/${this.state.patientName}`);
+        var dataArr = new Array();
         await query.once("value")
             .then(async snapshot => {
-                await snapshot.forEach(childSnapshot => {dataArr.unshift(childSnapshot.val()); console.log(1);});
+                await snapshot.forEach(childSnapshot => { dataArr.unshift(childSnapshot.val()); console.log(1); });
             })
-            .catch(() => this.setState({error: {message: 'Unexpected error'}}));
+            .catch(() => this.setState({ error: { message: 'Unexpected error' } }));
         console.log(dataArr);
         return dataArr;
     }
 
-    async displayData(dataArr){
-        dataArr=await dataArr;
-        var list=await dataArr.map(e => e.Prescription);
-        console.log(list);
-        return list;
+    async displayPatients() {
+        var patientList = await this.queryData(this.state.userName);
+        try {
+            var index=0;
+            this.setState({
+                patients: patientList.map((obj) => {
+                    return Object.keys(obj).map((key) => {
+                        index++;
+                        return <li key={index} className= "list-group-item"> {key}: {obj[key]} </li>;
+                    }); 
+                })
+            })
+            this.setState({clicked: false})
+        }
+        catch (err) {
+            this.setState({ error: { message: err } });
+        }
     }
 
-    render(){
+    componentDidMount() {
+        firebaseApp.auth().onAuthStateChanged(async user => {
+            if (user) {
+                await this.setState({ userName: user.displayName });
+            }
+        })
+    }
+
+    render() {
         console.log(this.state);
-        const displayScreen=(this.state.error.message === 'Enter a registered patient')?
-        (
-            <React.Fragment>
+        if (this.state.clicked) {
+            this.displayPatients();
+        }
+        const displayScreen = (this.state.error.message === 'Enter a registered patient') ?
+            (
+                <React.Fragment>
                     <h3> Enter patient name </h3>
                     <FormGroup controlId="formInLineName">
                         <ControlLabel>
@@ -51,30 +76,23 @@ class AccessData extends Component {
                             placeholder="Name" />
                     </FormGroup>{' '}
                     <Button
-                        onClick={ ()  => {
-                            firebaseApp.auth().onAuthStateChanged(async user => {
-                                if(user) {
-                                    await this.setState({userName: user.displayName});
-                                    this.setState({error: {message: `Displaying ${this.state.userName}'s patient ${this.state.patientName}'s data`}});
-                                }
-                            });
+                        onClick={async () => {
+                            await this.setState({ error: { message: `Displaying ${this.state.userName}'s patient ${this.state.patientName}'s data` } });
+                            await this.setState({ clicked: true });
                         }}
                         type="submit"
                     >
                         Search Name
                 </Button>
                 </React.Fragment>
-        ):
-        (
-            <div>
-            <React.Fragment>
-            {(this.displayData(this.queryData(this.state.userName, this.state.patientName)))}
-            </React.Fragment>
-            </div>
-        );
-        return(
+            ) :
+            (
+                null
+            );
+        return (
             <div>
                 {displayScreen}
+                {this.state.patients}
                 <div>{this.state.error.message}</div>
                 <div><Link to="./app"> Go back to main page </Link></div>
             </div>
