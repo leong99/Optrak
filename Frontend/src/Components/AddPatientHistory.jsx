df3385cf66745705fb3ba5249abb840fb0fb2ccc
Guillermo Leon <leon.guillermo1499@gmail.com>
2018-06-26 15:11:01 -0500

    Adds patient to patient registry stored on blockchain

--- Frontend/src/Components/AddPatientHistory.jsx
+++ Frontend/src/Components/AddPatientHistory.jsx
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Dropdown from 'react-dropdown';
-import {firebaseApp} from '../firebase';
+import { firebaseApp } from '../firebase';
 import 'react-dropdown/style.css';
 import { Link, BrowserRouter, Route } from 'react-router-dom';
-import {DropdownButton, MenuItem, Form, FormGroup, FormControl, ControlLabel, Button, Checkbox} from 'react-bootstrap';
+import { DropdownButton, MenuItem, Form, FormGroup, FormControl, ControlLabel, Button, Checkbox } from 'react-bootstrap';
 import Web3 from 'web3';
 import { contract } from './SignUp';
 
@@ -14,28 +14,49 @@ class AddPatientHistory extends Component {
         this.state = {
             userName: '',
             patientName: '',
-            prescription: '',
-            patientDosage: '',
-            lastPrescribedDate: '',
-            lastRefillDate: '',
+            uid: '',
             error: {
                 message: ''
             },
             overwrite: true
         };
-        
+
+    }
+
+    componentDidMount() {
+        firebaseApp.auth().onAuthStateChanged(user => {
+            if (!user) {
+                this.props.history.push('./signin');
+
+                //makes sure that the current user is signed in befor being able to access this page
+            }
+            else {
+                console.log(this.state.userName);
+                this.setState({ userName: user.displayName });
+            }
+
+        })
     }
 
     //Calls smart contract and adds different metadata to it. This is likely not the data that we are going to be using
     //But this can be modified easily to fit any data that need be stored on the blockchain
     //Current program assumes 1 opioid prescription to a patient
-    addPatientInfo = async() => {
-        firebaseApp.database().ref('Users/' + this.state.userName + '/Patients/' + this.state.patientName).push({
-            Dosage: this.state.patientDosage,
-            Prescription: this.state.prescription,
-            lastPrescribedDate : this.state.lastPrescribedDate,
-            lastRefillDate : this.state.lastRefillDate
-        })
+    addPatientInfo = async () => {
+        if (this.checkFields()) {
+            contract.then(optrakContract => {
+                //TODO: Verify unique uid
+                optrakContract.methods.addPatient(this.state.patientName, optrakContract.options.from, this.state.uid).send().then(result => {
+                    if (result) {
+                        alert('Patient succcessfully added to registry. Redirecting to main app now.')
+                        this.props.history.push('./App');
+                    }
+                    else {
+                        alert('Patient was not able to successfully be added. Please refresh and try again.');
+                    }
+                });
+            })
+        }
+
     }
 
     /**
@@ -43,31 +64,18 @@ class AddPatientHistory extends Component {
      */
     checkFields() {
         firebaseApp.auth().onAuthStateChanged(user => {
-            if(user) {
-                this.setState({userName: user.displayName});
+            if (user) {
+                this.setState({ userName: user.displayName });
             }
         })
-        
-        if(this.state.patientName == '') {
-            this.setState({error: {message: 'Please enter a patient name'}});
+
+        if (this.state.patientName == '') {
+            this.setState({ error: { message: 'Please enter a patient name' } });
             return false;
         }
         else {
-            if(this.state.prescription != '' && this.state.patientDosage == '') {
-                this.setState({error: {message: 'Please enter a dosage for the prescribed opioid'}});
-                return false;
-
-            }
-            else if(this.state.prescription == '' && this.state.patientDosage != '') {
-                this.setState({error: {message: 'Please enter an opioid for the given dosage'}});
-                return false;
-            }
-            else if(this.state.lastPrescribedDate == '') {
-                this.setState({error: {message: 'Please enter the last prescribed date'}});
-                return false;
-            }
-            else if(this.state.lastRefillDate == '') {
-                this.setState({error: {message: 'Please enter the last refill date'}});
+            if (this.state.uid == '') {
+                this.setState({ error: { message: 'Please enter a unique patient id' } });
                 return false;
             }
         }
@@ -76,43 +84,24 @@ class AddPatientHistory extends Component {
 
     render() {
         console.log(this.state);
-        firebaseApp.auth().onAuthStateChanged(user => {
-            if(!user) {
-                this.props.history.push('./signin');
-                //makes sure that the current user is signed in befor being able to access this page
-            }
-            else {
-                console.log(this.state.userName);
-            }
-            
-        })
-        return(
-        <div>
-            <h3>Enter patient information</h3>
-            <form inline='true'> 
-                <div className="form-group">
-                    <input type="text" placeholder="Enter Patient Name (Name on Records)" className="form-control" style={{marginRight: '5px'}}
-                    onChange={event => this.setState({patientName: event.target.value})}/>   
-                    <input type="text" placeholder="Enter Prescribed Dosage (xx mg)" className="form-control" style={{marginRight: '5px'}}
-                    onChange={event=> this.setState({patientDosage: event.target.value})}/>
-                    <input type="text" placeholder="Enter Prescribed Drug" className="form-control" style={{marginRight: '5px'}}
-                    onChange={event => this.setState({prescription: event.target.value})}/>
-                    <input type='text' placeholder="Enter Last Prescription Date (DD/MM/YYYY)" className="form-control" style={{marginRight:'5px'}}
-                    onChange={event => this.setState({lastPrescribedDate: event.target.value})}
-                    />
-                    <input type='text' placeholder="Enter Last Refill Date (DD/MM/YYYY)" className="form-control" syle={{marginRight: '5px'}}
-                    onChange={event => this.setState({lastRefillDate: event.target.value})}/>
-                    
-                    <button className="btn btn-success" type="button" onClick={() =>{this.addPatientInfo()}}>
-                    Submit
+
+        return (
+            <div>
+                <h3>Enter patient information</h3>
+                <form inline='true'>
+                    <div className="form-group">
+                        <input type="text" placeholder="Enter Patient Name (Name on Records)" className="form-control" style={{ marginRight: '5px' }}
+                            onChange={event => this.setState({ patientName: event.target.value })} />
+                        <input type="number" placeholder="Enter Patient's Unique ID" className="form-control" style={{ marginRight: '5px' }}
+                            onChange={event => this.setState({ uid: event.target.value })} />
+                        <button className="btn btn-success" type="button" onClick={() => { this.addPatientInfo() }}>
+                            Submit
                     </button>
-                    
-                    
-                </div>
-            </form>
-            <div>{this.state.error.message}</div>
-            <div><Link to="./app"> Go back to main page </Link></div>
-        </div>
+                    </div>
+                </form>
+                <div>{this.state.error.message}</div>
+                <div><Link to="./app"> Go back to main page </Link></div>
+            </div>
         )
     }
 }
